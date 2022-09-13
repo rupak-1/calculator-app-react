14034a541f29a094ed5cb6daa65a6300fb2b76d9
Rupak Kunwar <rupakkunwar11@gmail.com>
2022-09-13 15:41:49 -0500

    HTML-Wrote html codes

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+function App() {
+	const createDigits = () => {
+		const digits = [];
+		for(let i = 1; i < 10; i++){
+			digits.push(
+				<button key={i}>{i}</button>
+			)
+		}
+		return digits;
+	}
+	return (
+		<div className="App">
+			<div className="fi-calculator">
+				<div className="fi-display">
+					<span>(0)</span> 0
+				</div>
+				<div className="fi-operators">
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
+					<button>DEL</button>
+				</div>
+
+				<div className="fi-digits">
+					{createDigits()}
+					<button>0</button>
+					<button>.</button>
+					<button>=</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
